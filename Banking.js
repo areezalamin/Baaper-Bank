ec5e9fc6aa6e6a74945d09946409af988dad3b83
areezalamin <alaminmillat24434@gmail.com>
2022-06-12 11:32:07 +0600

    last commit

--- Banking.js
+++ Banking.js
@@ -1,46 +1,85 @@
-document.getElementById('deposite-button').addEventListener('click', function(){
-    const depositeButton = document.getElementById('deposite-input');
+function forInput(inputId){
+    const depositeButton = document.getElementById(inputId);
     const depositeButtonValueText = depositeButton.value;
     const depositeButtonValueNumber = parseFloat(depositeButtonValueText)
 
-    const depositeAmount = document.getElementById('deposite-amout')
+    depositeButton.value = '';
+
+    return depositeButtonValueNumber;
+
+}
+
+
+function forAmount(amountId, depositeButton) {
+    const depositeAmount = document.getElementById(amountId)
     const previousDepositeAmountText = depositeAmount.innerText;
     const previousDepositeAmountNumber = parseFloat(previousDepositeAmountText)
-    const newDepositeAmount =depositeButtonValueNumber + previousDepositeAmountNumber;
+    const newDepositeAmount = depositeButton + previousDepositeAmountNumber;
     depositeAmount.innerText = newDepositeAmount;
+    
+    return newDepositeAmount;
+
+}
 
 
-    const totalDepositeAmount = document.getElementById('total-amount')
-    const previousTotalDepositeAmountText = totalDepositeAmount.innerText;
-    const previousTotalDepositeAmountNumber = parseFloat(previousTotalDepositeAmountText);
-    const newTotalDepositeAmount = previousTotalDepositeAmountNumber + depositeButtonValueNumber;
-    totalDepositeAmount.innerText = newTotalDepositeAmount;
+document.getElementById('deposite-button').addEventListener('click', function(){
+    const depositeButton = forInput('deposite-input');
+
+    if (depositeButton >0) {
+        forAmount('deposite-amout', depositeButton);
+        forAmount('total-amount', depositeButton);
+    }
+    // console.log(depositeButton)
+    // const depositeButtonValueText = depositeButton.value;
+    // const depositeButtonValueNumber = parseFloat(depositeButtonValueText)
+
+    // const depositeAmount = forAmount('deposite-amout')
+    // const previousDepositeAmountText = depositeAmount.innerText;
+    // const previousDepositeAmountNumber = parseFloat(previousDepositeAmountText)
+    // const newDepositeAmount = depositeButton + previousDepositeAmountNumber;
+    // depositeAmount.innerText = newDepositeAmount;
+
+    
 
 
-    depositeButton.value = '';
+    // const totalDepositeAmount = document.getElementById('total-amount')
+    // const previousTotalDepositeAmountText = totalDepositeAmount.innerText;
+    // const previousTotalDepositeAmountNumber = parseFloat(previousTotalDepositeAmountText);
+    // const newTotalDepositeAmount = previousTotalDepositeAmountNumber + depositeButton ;
+    // totalDepositeAmount.innerText = newTotalDepositeAmount;
+
+    
+    
     
 })
 
 
 document.getElementById('withdrow-button').addEventListener('click', function(){
-    const withdrowButton = document.getElementById('withdrow-input');
-    const withdrowButtonValueText = withdrowButton.value;
-    const withdrowButtonValueNumber = parseFloat(withdrowButtonValueText)
+    const withdrowButton = forInput('withdrow-input');
 
-    const withdrowAmount = document.getElementById('withdraw-amount')
-    const previouswithdrowAmountText = withdrowAmount.innerText;
-    const previouswithdrowAmountNumber = parseFloat(previouswithdrowAmountText)
-    const newwithdrowAmount =withdrowButtonValueNumber + previouswithdrowAmountNumber;
-    withdrowAmount.innerText = newwithdrowAmount;
+    if (withdrowButton > 0) {
+
+        forAmount('withdraw-amount', withdrowButton)
+
+        const totalwithdrowAmount = document.getElementById('total-amount')
+        const previousTotalwithdrowAmountText = totalwithdrowAmount.innerText;
+        const previousTotalwithdrowAmountNumber = parseFloat(previousTotalwithdrowAmountText);
+        const newTotalwithdrowAmount = previousTotalwithdrowAmountNumber - withdrowButton;
+        totalwithdrowAmount.innerText = newTotalwithdrowAmount;
+        
+    }
+    // const withdrowButtonValueText = withdrowButton.value; 
+    // const withdrowButtonValueNumber = parseFloat(withdrowButtonValueText)
+
+    // const withdrowAmount = document.getElementById('withdraw-amount')
+    // const previouswithdrowAmountText = withdrowAmount.innerText;
+    // const previouswithdrowAmountNumber = parseFloat(previouswithdrowAmountText)
+    // const newwithdrowAmount = withdrowButton + previouswithdrowAmountNumber;
+    // withdrowAmount.innerText = newwithdrowAmount;
 
 
-    const totalwithdrowAmount = document.getElementById('total-amount')
-    const previousTotalwithdrowAmountText = totalwithdrowAmount.innerText;
-    const previousTotalwithdrowAmountNumber = parseFloat(previousTotalwithdrowAmountText);
-    const newTotalwithdrowAmount = previousTotalwithdrowAmountNumber - withdrowButtonValueNumber;
-    totalwithdrowAmount.innerText = newTotalwithdrowAmount;
 
 
-    withdrowButton.value = '';
+    // forAmount('total-amount', withdrowButton)
     
 })
